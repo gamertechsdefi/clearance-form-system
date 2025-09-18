3c68a5802823974ef26585e394b95290fcf81bdc
Gamer <gamertechs.defi@gmail.com>
2025-09-18 10:03:27 +0100

    fix

--- src/app/get-form/page.tsx
+++ src/app/get-form/page.tsx
@@ -59,7 +59,7 @@ export default function GetForm() {
   }, [formData.academicInfo.institutionType]);
 
   const validateStep = () => {
-    // let errors = { images: '' };
+    let errors = { images: '' };
     let isValid = true;
 
     if (step === 2) {
