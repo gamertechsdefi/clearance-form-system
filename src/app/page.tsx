efb9429dbc0362145638b39a32a51d505c469063
gamertechsdefi <gamertechs.defi@gmail.com>
2025-11-10 07:58:02 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,16 @@ export default function Home() {
           <h1 className="text-4xl font-bold">Clearance Form System</h1>
           <p className="text-lg">Get your clearance form online and print it out without hassle or stress</p>
           <Link href="/get-form" className="mt-4 px-4 py-2 bg-green-700 font-bold text-white rounded">Get Started</Link>
+
+          <div className="mt-8 flex flex-col items-center">
+            <p>DONE BY:</p>
+            <h1 className="flex gap-2 font-bold text-xl">
+              <span>23/305/01/F/0112,</span> 
+              <span>23/305/01/F/0153,</span> 
+              <span>20/69/0036 &</span> 
+              <span>20/69/0071</span>
+            </h1>
+          </div>
         </section>
       </main>
     </div>
